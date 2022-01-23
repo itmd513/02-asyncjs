ff49e846cb8b684409deb88078f6ab6dde11cc7a
Maria Vo <mvo1@hawk.iit.edu>
2022-01-23 15:17:54 -0600

    Add demo of setTimeout and nextTick

---
+++ nexttick.js
@@ -0,0 +1,9 @@
+function sayHello(who) {
+  return function() {
+    console.log(`Hello from ${who}!`);
+  }
+}
+
+setTimeout(sayHello('setTimeout'), 0);
+console.log(`Hello from nothing but source order!`);
+process.nextTick(sayHello('nextTick'));
